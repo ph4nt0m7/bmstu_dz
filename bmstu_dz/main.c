bf37c4317da684dc413623db22883bc421a59596
ph4nt0m7 <k@zhurov.com>
2019-09-02 14:35:29 +0300

    Create main.c

---
+++ bmstu_dz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  bmstu_dz
+//
+//  Created by Kostya on 02/09/2019.
+//  Copyright © 2019 ph4nt0m7. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
